72ff2a4649121999e63f103d1aaef7cf3c17dace
adepoju2006 <olanrewajuabdulrazaq5@gmail.com>
2021-05-20 16:35:05 +0100

    first commit

---
+++ object.js
@@ -0,0 +1,140 @@
+var gang={firstname:"Ayomide",
+          Surname:"Adepoju",
+          Class:"S.S.1",
+          Seatnumber:009,
+          Department:"Science"          
+};
+console.log(gang);
+
+var students=[
+    {firstname:"Daniel",
+    Surname:"Awute",
+    Class:"Primary 1",
+    Seatnumber:009,
+    Department:"Seience"          
+    },
+    {firstname:"Opeyemi",
+     Surname:"Adepoju",
+     Class:"Jamb",
+     Seatnumber:009,
+     Department:"Science"          
+},
+        {firstname:"Isreal",
+          Surname:"Victor",
+          Class:"Primary 2",
+          Seatnumber:029,
+          Department:"Art"          
+},
+        {firstname:"Ik",
+          Surname:"Chime",
+          Class:"S.S.1",
+          Seatnumber:119,
+          Department:"Commercial"          
+},
+        {firstname:"Aishat",
+          Surname:"Olaleye",
+          Class:"Primary 1",
+          Seatnumber:001,
+          Department:"Science"          
+},
+        {firstname:"Bunmi",
+          Surname:"Adepoju",
+          Class:"University",
+          Seatnumber:001,
+          Department:"Business"          
+},
+        {firstname:"Kemi",
+          Surname:"Adepoju",
+          Class:"primary 2",
+          Seatnumber:007,
+          Department:"Trader"          
+},
+        {firstname:"Paul",
+          Surname:"Adepoju",
+          Class:"Graduate",
+          Seatnumber:010,
+          Department:"Science"          
+},
+        {firstname:"Razak",
+          Surname:"Olawale",
+          Class:"S.S.1",
+          Seatnumber:020,
+          Department:"Commercial"          
+},
+        {firstname:"Gidion",
+          Surname:"Paul",
+          Class:"S.S.2",
+          Seatnumber:050,
+          Department:"Art"          
+},
+   
+];
+console.log(students);
+
+
+var students1=[
+    {firstname:"Daniel",
+    Surname:"Awute",
+    Class:"Primary 1",
+    Seatnumber:009,
+    Department:"Seience"          
+    },
+    {firstname:"Opeyemi",
+     Surname:"Adepoju",
+     Class:"Jamb",
+     Seatnumber:009,
+     Department:"Science"          
+},
+        {firstname:"Isreal",
+          Surname:"Victor",
+          Class:"Primary 2",
+          Seatnumber:029,
+          Department:"Art"          
+},
+        {firstname:"Ik",
+          Surname:"Chime",
+          Class:"S.S.1",
+          Seatnumber:119,
+          Department:"Commercial"          
+},
+        {firstname:"Aishat",
+          Surname:"Olaleye",
+          Class:"Primary 1",
+          Seatnumber:001,
+          Department:"Science"          
+},
+        {firstname:"Bunmi",
+          Surname:"Adepoju",
+          Class:"University",
+          Seatnumber:001,
+          Department:"Business"          
+},
+        {firstname:"Kemi",
+          Surname:"Adepoju",
+          Class:"primary 2",
+          Seatnumber:007,
+          Department:"Trader"          
+},
+        {firstname:"Paul",
+          Surname:"Adepoju",
+          Class:"Graduate",
+          Seatnumber:010,
+          Department:"Science"          
+},
+        {firstname:"Razak",
+          Surname:"Olawale",
+          Class:"S.S.1",
+          Seatnumber:020,
+          Department:"Commercial"          
+},
+        {firstname:"Gidion",
+          Surname:"Paul",
+          Class:"S.S.2",
+          Seatnumber:050,
+          Department:"Art"          
+},
+   
+];
+delete students1[5]
+delete students1[7]
+console.log(students1);
